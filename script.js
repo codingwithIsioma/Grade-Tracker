40604cc28a4fb995235c9a7dabff381b358f732a
Blessing Ezekiel <celezekiel02@gmail.com>
2026-05-15 23:14:15 +0100

    Add the functionality to edit and display the grade distribution

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const studentForm = document.getElementById("student-form");
 const nameInput = document.getElementById("student-name");
 const gradeInput = document.getElementById("student-grade");
+const submitBtn = document.getElementById("submit-btn");
 const errorMessage = document.getElementById("error-message");
 const averageValue = document.getElementById("average-value");
 const listContainer = document.getElementById("list-container");
@@ -10,9 +11,14 @@ const studentCount = document.getElementById("student-count");
 const student = document.getElementById("student");
 const sortAlphabetically = document.getElementById("sort-alphabetically");
 const sortByHighestGrade = document.getElementById("sort-by-highest-grade");
+const failCount = document.getElementById("fail-count");
+const passCount = document.getElementById("pass-count");
+const meritCount = document.getElementById("merit-count");
+const distinctionCount = document.getElementById("distinction-count");
 
 // main student data array
 let studentData = JSON.parse(localStorage.getItem("studentData")) || [];
+let currentDataObj = {};
 
 // removes the error when user puts in a valid input
 nameInput.addEventListener("input", (e) => {
@@ -21,7 +27,6 @@ nameInput.addEventListener("input", (e) => {
     showError("");
   }
 });
-
 gradeInput.addEventListener("input", (e) => {
   if (e.target.value) {
     gradeInput.classList.remove("error-border");
@@ -65,8 +70,43 @@ const refreshDisplay = () => {
   }
 };
 
+// update the grade distribution count
+const updateGradeDistributionCount = () => {
+  // fail
+  const failedStudents = studentData.filter(
+    (student) => student.grade >= 0 && student.grade <= 49,
+  );
+  failCount.textContent = failedStudents.length
+    ? `${failedStudents.length}`
+    : "0";
+
+  // pass
+  const passedStudents = studentData.filter(
+    (student) => student.grade >= 50 && student.grade <= 69,
+  );
+  passCount.textContent = passedStudents.length
+    ? `${passedStudents.length}`
+    : "0";
+
+  // merit
+  const meritedStudents = studentData.filter(
+    (student) => student.grade >= 70 && student.grade <= 84,
+  );
+  meritCount.textContent = meritedStudents.length
+    ? `${meritedStudents.length}`
+    : "0";
+
+  // distinction
+  const distinctionStudents = studentData.filter(
+    (student) => student.grade >= 85 && student.grade <= 100,
+  );
+  distinctionCount.textContent = distinctionStudents.length
+    ? `${distinctionStudents.length}`
+    : "0";
+};
+
 // handles the functionality of adding students to the student data array
-const addToStudentData = () => {
+const addOrUpdateToStudentData = () => {
   const capitalizedName = nameInput.value
     .trim()
     .split(" ")
@@ -74,26 +114,37 @@ const addToStudentData = () => {
     .map((name) => name[0].toUpperCase() + name.slice(1).toLowerCase())
     .join(" ");
 
-  studentData.push({
+  const findStudentDataIndex = studentData.findIndex(
+    (student) => student.id === currentDataObj.id,
+  );
+
+  const dataObj = {
     id: Date.now(),
     name: capitalizedName,
     grade: +gradeInput.value,
-  });
+  };
+
+  if (findStudentDataIndex === -1) {
+    studentData.push(dataObj);
+  } else {
+    studentData[findStudentDataIndex] = { ...dataObj, id: currentDataObj.id };
+  }
+
   localStorage.setItem("studentData", JSON.stringify(studentData));
   calculateAvgGrade();
-  refreshDisplay();
-  nameInput.value = "";
-  gradeInput.value = "";
+  checkArr();
+  reset();
 };
 
 // displays the student data onto the screen
 const displayStudentData = (studentData) => {
   const returnedGradeAverage = calculateAvgGrade();
+  listContainer.innerHTML = "";
+  let listHTML = "";
   if (studentData.length !== 0) {
-    listContainer.innerHTML = "";
     studentData.forEach(({ id, name, grade }, i) => {
-      listContainer.innerHTML += `
-        <div class="list-flex" id="${id}">
+      listHTML += `
+        <div class="list-flex">
             <div class="name-flex">
                 <span class="student-idx">${String(i + 1).padStart(2, "0")}</span>
                 <p>${name}</p>
@@ -103,51 +154,81 @@ const displayStudentData = (studentData) => {
             </div>
             <div class="grade-flex">
                 <p>${grade}</p>
-                <button class="delete-btn" onclick="deleteStudent(this)">
-                  <i class="fa-solid fa-xmark"></i>
-                </button>
+                <div class="student-buttons">
+                    <button class="edit-btn" onclick="editStudent(${id})">
+                    <i class="fa-regular fa-pen-to-square"></i>
+                    </button>
+                    <button class="delete-btn" onclick="deleteStudent(${id})">
+                    <i class="fa-solid fa-xmark"></i>
+                    </button>
+                </div>
             </div>
         </div>
       `;
     });
+    listContainer.innerHTML = listHTML;
+    updateGradeDistributionCount();
     studentLength.style.display = "flex";
     studentCount.textContent = `${studentData.length}`;
     student.textContent = `${studentData.length === 1 ? "student" : "students"}`;
+  } else {
+    listContainer.innerHTML = `
+    <div class="no-student">
+        <p>No students added yet.</p>  
+    </div>`;
+    studentLength.style.display = "none";
   }
 };
 
 // calculates the grade average of the students
 const calculateAvgGrade = () => {
-  let sum = 0;
-  studentData.map((student) => {
-    return (sum += +student.grade);
-  });
-  const gradeAverage = sum / studentData.length;
+  let initialValue = 0;
+  const sumOfGrades = studentData.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.grade,
+    initialValue,
+  );
+  const gradeAverage = studentData.length
+    ? sumOfGrades / studentData.length
+    : 0;
 
   // display average
   averageValue.textContent = gradeAverage ? `${gradeAverage.toFixed(1)}` : "—";
   return gradeAverage;
 };
 
+// edit button
+const editStudent = (buttonElId) => {
+  const studentDataArr = studentData.find(
+    (student) => student.id === +buttonElId,
+  );
+  currentDataObj = studentDataArr;
+  nameInput.value = currentDataObj.name;
+  gradeInput.value = currentDataObj.grade;
+
+  nameInput.classList.add("edit-border");
+  gradeInput.classList.add("edit-border");
+  submitBtn.innerText = "Save Changes";
+  submitBtn.classList.add("save-changes");
+};
+
 // delete button
-const deleteStudent = (buttonEl) => {
+const deleteStudent = (buttonElId) => {
   // find the index of the student to be deleted
   const studentDataArr = studentData.findIndex(
-    (student) => student.id === +buttonEl.parentElement.parentElement.id,
+    (student) => student.id === +buttonElId,
   );
-
   studentData.splice(studentDataArr, 1);
-  buttonEl.parentElement.parentElement.remove();
   localStorage.setItem("studentData", JSON.stringify(studentData));
   calculateAvgGrade();
-  refreshDisplay();
   checkArr();
+  reset();
 };
 
 // checks if the student list is empty or not
 const checkArr = () => {
+  listContainer.innerHTML = "";
   if (studentData.length === 0) {
-    listContainer.innerHTML += `
+    listContainer.innerHTML = `
     <div class="no-student">
         <p>No students added yet.</p>  
     </div>`;
@@ -160,6 +241,17 @@ const checkArr = () => {
 // displays the student list on loading of page whether empty or from local storage.
 checkArr();
 
+//reset
+const reset = () => {
+  submitBtn.innerText = "Add Student";
+  submitBtn.classList.remove("save-changes");
+  nameInput.classList.remove("edit-border");
+  gradeInput.classList.remove("edit-border");
+  nameInput.value = "";
+  gradeInput.value = "";
+  currentDataObj = {};
+};
+
 // sort function that updates the state of the sortfiler value, it is either, null, alpha or grade.
 sortAlphabetically.addEventListener("click", () => {
   if (activeSortFilter === "alpha") {
@@ -170,7 +262,7 @@ sortAlphabetically.addEventListener("click", () => {
     sortByHighestGrade.classList.remove("active");
     sortAlphabetically.classList.add("active");
   }
-  refreshDisplay();
+  checkArr();
 });
 sortByHighestGrade.addEventListener("click", () => {
   if (activeSortFilter === "grade") {
@@ -181,7 +273,7 @@ sortByHighestGrade.addEventListener("click", () => {
     sortAlphabetically.classList.remove("active");
     sortByHighestGrade.classList.add("active");
   }
-  refreshDisplay();
+  checkArr();
 });
 
 // handles submission of form
@@ -189,7 +281,7 @@ studentForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   if (validateNameAndGrade()) {
-    addToStudentData();
+    addOrUpdateToStudentData();
   }
 });
 
@@ -232,9 +324,18 @@ const validateNameAndGrade = () => {
   }
 
   // ensures that there are not duplicate names in the student list
-  const isDuplicate = studentData.filter(
-    (item) => item.name.toLowerCase() === nameInputValue.toLowerCase(),
-  );
+  const isDuplicate = studentData.filter((student) => {
+    if (currentDataObj.name !== undefined) {
+      if (student.name.toLowerCase() === currentDataObj.name.toLowerCase()) {
+        return false;
+      }
+    }
+
+    if (student.name.toLowerCase() === nameInputValue.toLowerCase()) {
+      return true;
+    }
+  });
+
   if (isDuplicate.length > 0) {
     showError(`"${nameInputValue}" is already in the list.`);
     return false;
